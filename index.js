1b619a72ecf3d45eb77fe3a5945f74d1e31eef21
Evgenii Rubin <kubikus2009@yandex.ru>
2023-04-04 18:37:08 +0300

    finish logic

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const PopupClose = document.querySelector('.popup__close');
-const Popup = document.querySelector('.popup');
+const textArray = [ // Header options
+    'Choose your weapon',
+    'Fighting...',
+    'Game results'
+]
 
-PopupClose.addEventListener('click', () => Popup.classList.add('popup_hide'));
+const popupClose = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
 const weaponChoice = ['Rock', 'Paper', 'Scissors'];
 const weaponRock = document.querySelector('.button_type_rock');
 const weaponPaper = document.querySelector('.button_type_paper');
 const weaponScissors = document.querySelector('.button_type_scissors');
 
-weaponRock.addEventListener('click', () => {
-    playerSelection = weaponChoice[0];
-    playRound();
-});
-weaponPaper.addEventListener('click', () => {
-    playerSelection = weaponChoice[1];
-    playRound();
-});
-weaponScissors.addEventListener('click', () => {
-    playerSelection = weaponChoice[2];
-    playRound(); 
-});
+const header = document.querySelector('.header');
+const headerText = document.querySelector('.header__text');
+const footer = document.querySelector('.footer');
 
 let computerSelection;
 let playerSelection;
@@ -34,21 +29,21 @@ function playRound() { // Start new round of game
     computerSelection = computerPlay();
     
     let result = checkWinner(playerSelection, computerSelection);
-    console.log(result);
-    console.log(computerSelection);
 
     if (result.match('Win')) {
         computerWin--;
-        window.alert(`${result}. ${playerWin} : ${computerWin}`); // Computer lose one live
+        addBattleText(`${result}. ${playerWin} : ${computerWin}`); // Computer lose one live
         checkCount();
     } else if (result.match('Lose')) {
         playerWin--;
-        window.alert(`${result}. ${playerWin} : ${computerWin}`); // Player lose one live
+        addBattleText(`${result}. ${playerWin} : ${computerWin}`); // Player lose one live
         checkCount();
-    } else return window.alert(result);
+    } else return addBattleText(result);
+
+    changeHeaderText(textArray[0]);
 }
 
-function checkWinner(player, computer) { // Check the winner of a round
+function checkWinner(player, computer) { // Check the winner of the round
     if ((player === weaponChoice[0] && computer === weaponChoice[1]) || 
     (player === weaponChoice[1] && computer === weaponChoice[2]) ||
     (player === weaponChoice[2] && computer === weaponChoice[0])) {
@@ -62,13 +57,59 @@ function checkWinner(player, computer) { // Check the winner of a round
 
 function checkCount() {
     if (computerWin === 0 || playerWin === 0) {
-        return gameCount(); 
+        return gameCount();
     }
 }
 
 function gameCount() { // Count game result
+    changeHeaderText(textArray[2]);
+
     if (playerWin > computerWin) {
         window.alert(`${playerWin} : ${computerWin}. You Win! Computer is defeated!`)
     } else window.alert(`${playerWin} : ${computerWin}. You Lose! Try one more time:(`);
-    return Popup.classList.remove('popup_hide');
+
+    computerWin = 5;
+    playerWin = 5;
+    
+    const footerChildren = document.querySelectorAll('.footer__text'); // Clean battlefield
+    footerChildren.forEach( item => item.remove());
+
+    return popup.classList.remove('popup_hide');
 }
+
+function changeHeaderText(text) { // Change header text
+    console.log(headerText);
+    headerText.textContent = '';
+    headerText.textContent = text;
+}
+
+function addBattleText(text) { // log battle
+    const p = document.createElement('p');
+    p.classList.add('footer__text');
+    p.textContent = text;
+
+    footer.appendChild(p);
+
+    changeHeaderText(textArray[0]);
+}
+
+popupClose.addEventListener('click', () => {
+    popup.classList.add('popup_hide');
+    changeHeaderText(textArray[0]);
+});
+
+weaponRock.addEventListener('click', () => {
+    playerSelection = weaponChoice[0];
+    setTimeout(() => playRound(), 1000);
+    changeHeaderText(textArray[1]);
+});
+weaponPaper.addEventListener('click', () => {
+    playerSelection = weaponChoice[1];
+    setTimeout(() => playRound(), 1000);
+    changeHeaderText(textArray[1]);
+});
+weaponScissors.addEventListener('click', () => {
+    playerSelection = weaponChoice[2];
+    setTimeout(() => playRound(), 1000);
+    changeHeaderText(textArray[1]);
+});
